755e0946752c8967ff0592444ea9b70b2fc119e9
mabbasjafrisyed <74119853+mabbasjafrisyed@users.noreply.github.com>
2021-03-29 19:38:58 -0400

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,22 @@
+class Paper{
+    constructor(x, y, radius){
+        var options = {
+            restitution:0.3,
+            density:1.2
+        }
+        this.image=loadImage("paper.png");
+        this.body = Bodies.circle(x, y, radius ,options);
+        this.radius = radius;
+        World.add(world, this.body);
+    }
+
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        image(this.image, 0, 0, this.radius+10, this.radius+10)
+        pop();
+    }
+}
